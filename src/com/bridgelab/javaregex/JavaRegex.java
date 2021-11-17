62042b11aa7a0c53a38942e9d5edbfd90ef3eae3
Tutorials-Git <ankireddy905292@gmail.com>
2021-11-17 19:17:47 +0530

    first commit Java Regex

--- src/com/bridgelab/javaregex/JavaRegex.java
+++ src/com/bridgelab/javaregex/JavaRegex.java
@@ -2,16 +2,20 @@ package com.bridgelab.javaregex;
 import java.util.Scanner;
 import java.util.regex.*;
 
-public class JavaRegex {
-    public void mobileNum() {
+public class JavaRegex{
+
+    // rule1  minimum 8 characters
+
+    public void passWordRule1() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Mobile number with country code : ");
-        String mobileNum = sc.nextLine();
-        boolean check = Pattern.matches("^91\\s[6789][0-9]{9}", mobileNum);
+        System.out.print("Enter password :  ");
+        String passWord = sc.nextLine();
+        boolean check = Pattern.matches("[a-zA-Z]{8,}", passWord);
         if (check)
-            System.out.println("Valid Mobile Number ");
+            System.out.println("Valid PassWord");
         else
-            System.out.println("Please Enter a Valid Mobile Num ex:- 91 9052824862");
+            System.out.println("Please Enter a Valid password, it should have minimum 8 characters");
+
     }
 
     public static void main(String[] args) {
@@ -20,10 +24,6 @@ public class JavaRegex {
 
         System.out.println("Welcome to User Registration Program");
 
-
-        user.mobileNum();
+        user.passWordRule1();
     }
 }
-
-
-

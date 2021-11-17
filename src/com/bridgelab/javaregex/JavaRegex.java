a850a51ba5289ec6ef3bce0802ff8b87222a5430
Tutorials-Git <ankireddy905292@gmail.com>
2021-11-17 19:24:24 +0530

    first commit Java Regex

--- src/com/bridgelab/javaregex/JavaRegex.java
+++ src/com/bridgelab/javaregex/JavaRegex.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class JavaRegex{
-    // rule2  minimum 8 characters with 1 upper case
-    public void passWordRule2() {
+    // rule2  minimum 8 characters with, 1 upper case and 1 Numeric
+    public void passWordRule3() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter password :  ");
         String passWord = sc.nextLine();
-        boolean check = Pattern.matches("[A-Z]{1}[a-z]{7,}", passWord);
+        boolean check = Pattern.matches("[A-Z]{1}[a-z]{6,}[0-9]{1}", passWord);
         if (check)
             System.out.println("Password Valid");
         else
-            System.out.println("Please Enter a Valid password, it should have minimum 8 characters with 1 upper case");
+            System.out.println("Please Enter a Valid password, it should have minimum 8 characters with, 1 upper case and 1 Numeric ");
     }
 
     public static void main(String[] args) {
@@ -21,6 +21,10 @@ public class JavaRegex{
 
         System.out.println("Welcome to User Registration Program");
 
-        user.passWordRule2();
+
+        user.passWordRule3();
+
+
     }
 }
+

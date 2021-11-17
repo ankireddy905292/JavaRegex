2f62c03833494bb78c21181a1e0ca1cc47c913e9
Tutorials-Git <ankireddy905292@gmail.com>
2021-11-17 19:01:49 +0530

    first commit Java Regex

--- src/com/bridgelab/javaregex/JavaRegex.java
+++ src/com/bridgelab/javaregex/JavaRegex.java
@@ -1,7 +1,27 @@
 package com.bridgelab.javaregex;
+import java.util.Scanner;
+import java.util.regex.*;
 
 public class JavaRegex {
-    public static void main(String[] args) {
-        System.out.println("Welcome Java Regex");
+    Scanner sc = new Scanner(System.in);
+    public void firstNameCheck() {
+        System.out.print("Enter First Name of User: ");
+        String fName = sc.next();
+        boolean check = Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", fName);
+        if (check)
+            System.out.println("Valid First Name");
+        else
+            System.out.println("Please Enter a Valid First name with only \"One Starting Capital\" letter");
+    }
+
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        JavaRegex user = new JavaRegex();
+
+        System.out.println("Welcome to User Registration Program");
+
+        user.firstNameCheck();
     }
 }
+
+
